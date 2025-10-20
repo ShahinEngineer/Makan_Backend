a3d278a8f9a787da7c47b894b5c9319788665e0a
ShahinEngineer <41575336+ShahinEngineer@users.noreply.github.com>
2025-10-20 21:52:12 +0300

    edit home api

--- app/routes/homePage.py
+++ app/routes/homePage.py
@@ -3,6 +3,9 @@ from app.db.session import get_db
 from app.lib.new import get_featured_news
 from app.lib.product import get_featured_products, get_all_products
 from app.lib.category import get_all_categories, get_categories_with_product_images
+from app.lib.partner import get_all_partners
+from app.lib.gallary import getAllGallary
+from app.lib.team import getAll_team
 
 router = APIRouter()
 
@@ -13,12 +16,18 @@ def get_all_info(db=Depends(get_db)):
         featured_products = get_featured_products(db)
         categories_with_product_image = get_categories_with_product_images(db)
         featured_news = get_featured_news(db)
+        partners = get_all_partners(db)
+        gallary = getAllGallary(db)
+        team = getAll_team(db)
 
         return {
             "all_product": all_product,
             "featured_products": featured_products,
             "categories": categories_with_product_image,
-            "featured_news": featured_news
+            "featured_news": featured_news,
+            "partners": partners,
+            "gallary": gallary,
+            "teams": team,
         }
 
     except Exception as e:
